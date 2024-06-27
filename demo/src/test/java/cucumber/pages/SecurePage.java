785fa7c21d4c02c08d6dff8293f42556902d8d1f
Luiza Graciano <luizagraciano.m@gmail.com>
2024-06-26 21:13:27 -0300

    constroi o page object secure area

--- demo/src/test/java/cucumber/pages/SecurePage.java
+++ demo/src/test/java/cucumber/pages/SecurePage.java
@@ -1,11 +1,20 @@
 package cucumber.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class SecurePage extends BasePage{
 
     public SecurePage(WebDriver navegador) {
         super(navegador);
+    }
+
+    public boolean paginaCerta(String urlEsperada){
+        return navegador.getCurrentUrl().equals(urlEsperada);
+    }
+
+    public void clicarXpath(String xpath){
+    navegador.findElement(By.xpath(xpath)).click();
     }
     
 }
