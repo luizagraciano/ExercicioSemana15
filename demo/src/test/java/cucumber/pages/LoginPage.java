6353587614e5e8c9e7a5f621583daad4f5656f3e
Luiza Graciano <luizagraciano.m@gmail.com>
2024-06-26 21:12:04 -0300

    constroi o page object login

--- demo/src/test/java/cucumber/pages/LoginPage.java
+++ demo/src/test/java/cucumber/pages/LoginPage.java
@@ -1,11 +1,23 @@
 package cucumber.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver navegador) {
         super(navegador);
+    }
+
+    WebElement loginForm = navegador.findElement(By.id("login"));
+
+    public void preencherId(String id, String dado){
+        loginForm.findElement(By.id(id)).sendKeys(dado);
+    }
+
+    public void clicarXpath(String xpath){
+        loginForm.findElement(By.xpath(xpath)).click();
     }
     
 }
